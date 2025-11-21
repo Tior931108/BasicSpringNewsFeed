86466b6bd9c59056b792636d2521874de48bc705
Tior931108 <jyj8469@gmail.com>
2025-11-21 14:15:22 +0900

    Initial commit

---
+++ src/main/java/com/example/basicspringnewsfeed/BasicSpringNewsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicspringnewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringNewsFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSpringNewsFeedApplication.class, args);
+    }
+
+}
