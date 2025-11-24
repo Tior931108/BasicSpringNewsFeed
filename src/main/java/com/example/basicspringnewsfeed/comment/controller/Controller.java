e77ca24f2860664b5dd4e9ab3908494541a0394c
Leechungwoon <dlcjddns22@gmail.com>
2025-11-24 18:18:44 +0900

    ✨ feat: 생성, 조회(단건, 다건) 기능 추가
    생성, 조회(단건, 다건) 기능 추가
    포스트맨 작동 검수 완료

---
+++ src/main/java/com/example/basicspringnewsfeed/comment/controller/Controller.java
@@ -0,0 +1,48 @@
+package com.example.basicspringnewsfeed.comment.controller;
+
+import com.example.basicspringnewsfeed.comment.dto.request.CommentCreateRequest;
+import com.example.basicspringnewsfeed.comment.dto.response.CommentCreateResponse;
+import com.example.basicspringnewsfeed.comment.dto.response.CommentGetResponse;
+import com.example.basicspringnewsfeed.comment.dto.response.CommentUpdateResponse;
+import com.example.basicspringnewsfeed.comment.repository.CommentRepository;
+import com.example.basicspringnewsfeed.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
+public class Controller {
+    private final CommentRepository commentRepository;
+    private final CommentService commentService;
+
+    @PostMapping("/{postId}/{userId}")
+    public ResponseEntity<CommentCreateResponse> commentCreate(
+            @PathVariable Long postId,
+            @PathVariable Long userId,
+//            @AuthenticationPrincipal 추가 예정 //JWT시큐리티 관련
+            @RequestBody CommentCreateRequest request) {
+
+        return ResponseEntity.ok().body(commentService.save(postId, userId, request));
+    }
+
+    // 단건조회
+    @GetMapping("/{commentId}")
+    public ResponseEntity<CommentGetResponse> commentGet(
+            @PathVariable Long commentId) {
+        return ResponseEntity.ok().body(commentService.commentOneGet(commentId));
+    }
+
+    //다 건 조회
+    @GetMapping
+    public ResponseEntity<List<CommentGetResponse>> commentAllGet() {
+        List<CommentGetResponse> responses = commentService.commentAllGet();
+        {
+            return ResponseEntity.ok().body(responses);
+        }
+    }
+}
+
