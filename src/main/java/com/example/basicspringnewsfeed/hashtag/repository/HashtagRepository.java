6989d2a46887410c65a0120760c79c6154719098
castlejuyeon <jucess1106@naver.com>
2025-11-26 12:02:06 +0900

    Feat: HashtagRepository 기본 조회 메서드 추가
    - findByHashtagName(String) : 이름으로 해시태그 조회
    - (주석) findTop10ByHashtagCount() : 상위 10개 해시태그 조회, 테스트 후 구현 예정

--- src/main/java/com/example/basicspringnewsfeed/hashtag/repository/HashtagRepository.java
+++ src/main/java/com/example/basicspringnewsfeed/hashtag/repository/HashtagRepository.java
@@ -2,6 +2,15 @@ package com.example.basicspringnewsfeed.hashtag.repository;
 
 import com.example.basicspringnewsfeed.hashtag.entity.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+    Optional<Hashtag> findByHashtagName(String hashtagName);
+
+//    모든 테스트 완료후 문제 없을 때 구현
+//    @Query("SELECT h FROM Hashtag h ORDER BY h.hashtagCount DESC LIMIT 10")
+//    List<Hashtag> findTop10ByHashtagCount();
 }
